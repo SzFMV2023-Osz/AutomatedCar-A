47d21c5d8d78a4dea8762a6106f67bdd6b7fae52
SKincso <suranyi.kincso@gmail.com>
2023-10-18 20:17:26 +0200

    Implement IMovementVectorPacket interface and MovementVectorPacket

---
+++ src/AutomatedCar/SystemComponents/Packets/MovementVectorPacket.cs
@@ -0,0 +1,18 @@
+﻿namespace AutomatedCar.SystemComponents.Packets
+{
+    public interface IMovementVectorPacket
+    {
+        public (int, int) MovementVector { get; }
+    }
+
+    public class MovementVectorPacket : IMovementVectorPacket
+    {
+        private (int, int) movementVector;
+
+        public (int, int) MovementVector
+        {
+            get { return movementVector; }
+            set { movementVector = value; }
+        }
+    }
+}
