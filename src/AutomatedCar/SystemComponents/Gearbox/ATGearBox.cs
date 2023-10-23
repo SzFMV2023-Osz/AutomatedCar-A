c11e1fa3b2830ac95d0607ab521d613c9fdf7eb2
Astrokrisz <biro.krisztian.tamas@gmail.com>
2023-10-24 00:07:08 +0200

    ATGearBox skeletone created

---
+++ src/AutomatedCar/SystemComponents/Gearbox/ATGearBox.cs
@@ -0,0 +1,24 @@
+﻿namespace AutomatedCar.SystemComponents.Gearbox
+{
+    using AutomatedCar.Helpers.Gearbox_helpers;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class ATGearBox : IGearBox
+    {
+        private ATGears gear;
+        public int Velocity { get; set; }
+        public string GearStage { get; private set; }
+        public void CalculateGearSpeed(ref int revolution)
+        {
+            throw new NotImplementedException();
+        }
+        public void ShiftingGear(GearShift shift)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
