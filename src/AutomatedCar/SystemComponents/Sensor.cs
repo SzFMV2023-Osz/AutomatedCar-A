4b2e6a2582b4be37297393a63efe7ce62d34ec22
Zsebtelep <botosgergo98@gmail.com>
2023-10-23 20:26:36 +0200

    Create Sensor.cs
    Created Sensor.cs and properties

---
+++ src/AutomatedCar/SystemComponents/Sensor.cs
@@ -0,0 +1,19 @@
+﻿namespace AutomatedCar.SystemComponents
+{
+    using AutomatedCar.Models;
+    using Avalonia.Media;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    interface Sensor
+    {
+        public double viewAngle { get; set; }
+        public double viewDistance { get; set; }
+        public List<WorldObject> currentObjectinView { get; set; }
+        public PolylineGeometry sensorTriangle { get; set; }
+
+    }
+}
