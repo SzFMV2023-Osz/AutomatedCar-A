e0ab54a066348f13b3d4069536e83bb7c47da1c1
DanShepherd27 <jdani20001027@gmail.com>
2023-10-26 02:09:56 +0200

    Created KeyboardHandlerPacket

---
+++ src/AutomatedCar/SystemComponents/Packets/KeyboardHandlerPacket.cs
@@ -0,0 +1,29 @@
+﻿namespace AutomatedCar.SystemComponents.Packets
+{
+    using ReactiveUI;
+
+    public class KeyboardHandlerPacket : ReactiveObject, IReadOnlyKeyboardHandlerPacket
+    {
+        private int brakePercentage;
+        private int throttlePercentage;
+        private int wheelPercentage;
+
+        public int BrakePercentage
+        {
+            get => this.brakePercentage;
+            set => this.RaiseAndSetIfChanged(ref this.brakePercentage, value);
+        }
+
+        public int ThrottlePercentage
+        {
+            get => this.throttlePercentage;
+            set => this.RaiseAndSetIfChanged(ref this.throttlePercentage, value);
+        }
+
+        public int WheelPercentage
+        {
+            get => this.wheelPercentage;
+            set => this.RaiseAndSetIfChanged(ref this.wheelPercentage, value);
+        }
+    }
+}
