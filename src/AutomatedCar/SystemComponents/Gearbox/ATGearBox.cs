877d57b438ea0f8a7fef5b2f3094665e9615d503
Astrokrisz <biro.krisztian.tamas@gmail.com>
2023-10-28 19:04:26 +0200

    Documentation added

--- src/AutomatedCar/SystemComponents/Gearbox/ATGearBox.cs
+++ src/AutomatedCar/SystemComponents/Gearbox/ATGearBox.cs
@@ -7,12 +7,15 @@
     using System.Text;
     using System.Threading.Tasks;
 
+    /// <summary>
+    /// This is the Automatic Transmission class.
+    /// </summary>
     public class ATGearBox : IGearBox
     {
         private double[] gearRatios = { 0.002, 0.005, 0.01, 0.015, 0.0225, 0.027 };
         private int currentInsideGearStage = 0;
         private int nextLowRevolutionChangeValue = 1000;
-        public int Velocity { get; set; }
+        public int Velocity { get; set; } // velocity = revolution * gearRation
         public ATGears GearStage { get; private set; }
         public int CalculateGearSpeed(int revolution, int enginespeed)
         {
@@ -29,6 +32,12 @@
             }
         }
 
+        /// <summary>
+        /// This method calculate the Reverse mode velocity and engine revolution.
+        /// </summary>
+        /// <param name="revolution">The currently engine revolution.</param>
+        /// <param name="enginespeed"> The engine torque by gas pedal.</param>
+        /// <returns>The engine's new revolution.</returns>
         private int ReverseCalculate(int revolution, int enginespeed)
         {
             revolution = ModifyRevolution(revolution, enginespeed);
@@ -36,6 +45,12 @@
             return revolution;
         }
 
+        /// <summary>
+        /// This method calculate the Neutral mode engine revolution.
+        /// </summary>
+        /// <param name="revolution">The currently engine revolution.</param>
+        /// <param name="enginespeed"> The engine torque by gas pedal.</param>
+        /// <returns>The engine's new revolution.</returns>
         private int NeutralCalculate(int revolution, int enginespeed)
         {
             if (enginespeed > 0 && (revolution < enginespeed + 1000))
@@ -46,27 +61,37 @@
             return SlowsDownRevolution(revolution);
         }
 
+        /// <summary>
+        /// This method calculate the Drive mode velocity and engine revolution.
+        /// </summary>
+        /// <param name="revolution">The currently engine revolution.</param>
+        /// <param name="enginespeed"> The engine torque by gas pedal.</param>
+        /// <returns>The engine's new revolution.</returns>
         private int DriveCalculate(int revolution, int enginespeed)
         {
             if ((revolution > nextLowRevolutionChangeValue || currentInsideGearStage == 1) && (revolution <= 4000 || currentInsideGearStage == gearRatios.Length - 1))
             {
                 revolution = ModifyRevolution(revolution, enginespeed);
             }
-            else if (revolution <= nextLowRevolutionChangeValue)
+            else if (revolution <= nextLowRevolutionChangeValue) 
             {
+                // changing to lower gears
                 --currentInsideGearStage;
                 revolution = (int)(Velocity / gearRatios[currentInsideGearStage]) + 1000;
                 if (currentInsideGearStage - 1 > 0)
                 {
+                    // if not in first gear
                     nextLowRevolutionChangeValue = (int)(4000 * gearRatios[currentInsideGearStage - 1] / gearRatios[currentInsideGearStage]) - 500;
                 }
                 else
                 {
+                    // in a first gear
                     nextLowRevolutionChangeValue = 1000;
                 }
             }
-            else
+            else 
             {
+                // changing to higher gears
                 ++currentInsideGearStage;
                 revolution = (int)(Velocity / gearRatios[currentInsideGearStage]) + 1000;
                 nextLowRevolutionChangeValue = revolution - 500;
@@ -76,6 +101,13 @@
             return revolution;
         }
 
+        /// <summary>
+        /// This method calculate the engine revolution.
+        /// Monitors velocity changes (if changed calculate a new revolution based by velocity).
+        /// </summary>
+        /// <param name="revolution">The currently engine revolution.</param>
+        /// <param name="enginespeed"> The engine torque by gas pedal.</param>
+        /// <returns>The engine's new revolution.</returns>
         private int ModifyRevolution(int revolution, int enginespeed)
         {
             if (enginespeed > 0 && (revolution < enginespeed + 1000))
@@ -84,7 +116,7 @@
             }
             else
             {
-                if (Math.Abs(Velocity) != (int)((revolution - 1000) * gearRatios[currentInsideGearStage]))
+                if (Math.Abs(Velocity) != (int)((revolution - 1000) * gearRatios[currentInsideGearStage])) // aka has break input
                 {
                     revolution = (int)(Math.Abs(Velocity) / gearRatios[currentInsideGearStage]) + 1000;
                 }
@@ -93,12 +125,27 @@
             }
         }
 
+        /// <summary>
+        /// Calculate the engine revolution by currently gearstage.
+        /// </summary>
+        /// <param name="revolution">The currently engine revolution.</param>
+        /// <param name="enginespeed"> The engine torque by gas pedal.</param>
+        /// <returns>The engine's new revolution.</returns>
         private int CalculateRevolution(int revolution, int enginespeed)
         {
             double throttenmultiply = (1 - ((double)revolution) / (enginespeed + 1000));
             return (int)(revolution + (enginespeed * throttenmultiply) * gearRatios[gearRatios.Length - 1 - currentInsideGearStage] / 1.5);
         }
 
+        // throttenmultiply -> normally the value is between (0,1) , if the torque is higher then revolution it can be negative number, but this was not used.
+        // return value -> is similar for f(x) = -1/x
+        // gearRatios[gearRatios.Length - 1 - currentInsideGearStage] / 1.5 --> time controling number, how long takes to convergate to borders, invert gearratio (because it is work fine XD)
+
+        /// <summary>
+        /// Slow-down the engine revolution.
+        /// </summary>
+        /// <param name="revolution">The currently engine revolution.</param>
+        /// <returns>The engine's new revolution.</returns>
         private int SlowsDownRevolution(int revolution)
         {
             int newRevolution = revolution - revolution / (GearStage == ATGears.N ? 30 : 600);
@@ -119,12 +166,12 @@
             {
                 if (GearStage == ATGears.R)
                 {
-                    if (nextGearStage == ATGears.N)
+                    if (nextGearStage == ATGears.N) // if shifting to the Neutral mode
                     {
                         GearStage = nextGearStage;
                         currentInsideGearStage = 0;
                     }
-                    else if (Velocity == 0)
+                    else if (Velocity == 0) // if shifting to the Park mode
                     {
                         GearStage = nextGearStage;
                     }
@@ -132,13 +179,14 @@
                 }
                 else if (GearStage == ATGears.N)
                 {
+                    // for to not change direction in moving immediately while changing from Neutral
                     if ((nextGearStage == ATGears.R && Velocity <= 0) || (nextGearStage == ATGears.D && Velocity >= 0))
                     {
                         GearStage = nextGearStage;
                         currentInsideGearStage = 1;
                     }
                 }
-                else
+                else // Park -> Reverse, Drive -> Neutral
                 {
                     GearStage = nextGearStage;
                     if (GearStage == ATGears.N)
