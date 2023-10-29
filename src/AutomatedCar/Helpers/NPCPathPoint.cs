4ca3887d75d8f9115094f93945259b244379e243
NKadosa <nagykadosa@gmx.com>
2023-10-29 15:42:28 +0100

    Added NPCPathPoints class to store path data from JSON

---
+++ src/AutomatedCar/Helpers/NPCPathPoint.cs
@@ -0,0 +1,32 @@
+﻿namespace AutomatedCar.Helpers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class NPCPathPoint
+    {
+        private int x;
+        private int y;
+        private double rotation;
+        private int speed;
+
+        public NPCPathPoint(int x, int y, double rotation, int speed)
+        {
+            this.x = x;
+            this.y = y;
+            this.rotation = rotation;
+            this.speed = speed;
+        }
+
+        public int X { get => this.x; }
+
+        public int Y { get => this.y; }
+
+        public double Rotation { get => this.rotation; }
+
+        public int Speed { get => this.speed; }
+    }
+}
