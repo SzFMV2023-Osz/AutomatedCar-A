bfca2fce563c015df4b9fe7b6d75b82d6df33995
Zsebtelep <botosgergo98@gmail.com>
2023-10-31 11:55:08 +0100

    Revers

--- src/AutomatedCar/SystemComponents/Camera.cs
+++ src/AutomatedCar/SystemComponents/Camera.cs
@@ -11,7 +11,7 @@
     {
         public int SensorPositionX {get;set;}
         public int CameraPositionY { get; set; }
-        public WorldObject highlightedObjectCamera { get; set; }
+        
 
         public Camera(double ViewAngle, double ViewDistance)
         {
@@ -23,21 +23,6 @@
             this.currentObjectinView.Add(worldObject);
         }
 
-        public void ClosestHighlightedObject(AutomatedCar car)
-        {
-            this.highlightedObjectCamera = currentObjectinView[0];
-            for (int i = 0; i < currentObjectinView.Count - 1; i++)
-            {
-                if (CalculateDistance(currentObjectinView[i].X, currentObjectinView[i].Y, car.X, car.Y) <= CalculateDistance(currentObjectinView[i + 1].X, currentObjectinView[i + 1].Y, car.X, car.Y))
-                {
-                    this.highlightedObjectCamera = currentObjectinView[i];
-                }
-            }
-        }
-        private double CalculateDistance(double xACoordinate, double yACoordinate, double xBCoordinate, double yBCoordinate)
-        {
-            double distance = Math.Sqrt(Math.Pow((xBCoordinate - xACoordinate), 2) + Math.Pow((yBCoordinate - yACoordinate), 2));
-            return distance;
-        }
+       
     }
 }
--- src/AutomatedCar/SystemComponents/Sensor.cs
+++ src/AutomatedCar/SystemComponents/Sensor.cs
@@ -14,15 +14,30 @@
         public double viewDistance { get; protected set; }
         public List<WorldObject> currentObjectinView { get; protected set; }
         public PolylineGeometry sensorTriangle { get;protected set; }
+        public WorldObject highlightedObject { get; set; }
 
-        
         public void SensorTriangleUpdate()
         {
 
         }
-        
+        public void ClosestHighlightedObject(AutomatedCar car)
+        {
+            this.highlightedObject = currentObjectinView[0];
+            for (int i = 0; i < currentObjectinView.Count - 1; i++)
+            {
+                if (CalculateDistance(currentObjectinView[i].X, currentObjectinView[i].Y, car.X, car.Y) <= CalculateDistance(currentObjectinView[i + 1].X, currentObjectinView[i + 1].Y, car.X, car.Y))
+                {
+                    this.highlightedObject = currentObjectinView[i];
+                }
+            }
+        }
+        private double CalculateDistance(double xACoordinate, double yACoordinate, double xBCoordinate, double yBCoordinate)
+        {
+            double distance = Math.Sqrt(Math.Pow((xBCoordinate - xACoordinate), 2) + Math.Pow((yBCoordinate - yACoordinate), 2));
+            return distance;
+        }
 
-        
+
 
     }
 }
