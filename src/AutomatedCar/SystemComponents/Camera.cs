30f045181ddc6998b8077af8c2bcdc3f71685414
Zsebtelep <botosgergo98@gmail.com>
2023-10-31 11:42:31 +0100

    Camera class created
    Created the camera class and modified the Sensor class

--- src/AutomatedCar/SystemComponents/Camera.cs
+++ src/AutomatedCar/SystemComponents/Camera.cs
@@ -1,51 +1,36 @@
 ﻿namespace AutomatedCar.SystemComponents
 {
     using AutomatedCar.Models;
-    using Avalonia;
-    using Avalonia.Media;
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
 
-    internal class Radar : Sensor
-    {  
-        //Would it be better to be implemented in the Sensor class?
-       
+    class Camera : Sensor
+    {
+        public int SensorPositionX {get;set;}
+        public int CameraPositionY { get; set; }
+        public WorldObject highlightedObjectCamera { get; set; }
 
-        public int RadarPositionX { get; set; }
-        public int RadarPositionY { get; set; }
-        public WorldObject highlightedObject { get; private set; }
-
-        // It's not necessary, should be a protected setter be better? 
-        public Radar (double ViewAngle, double ViewDistance) 
+        public Camera(double ViewAngle, double ViewDistance)
         {
             ViewAngle = 60;
-            ViewDistance = 200;
+            ViewDistance = 80;
         }
-
         public void ObjectInRange (WorldObject worldObject)
         {
             this.currentObjectinView.Add(worldObject);
         }
 
-        //Is this calculation also needed in the camera class? 
-        
-
-        //figure out how the PolylineGeometry works
-        public void VisualiseRadarVision()
-        {
-            //this.sensorTriangle
-        }
         public void ClosestHighlightedObject(AutomatedCar car)
         {
-            this.highlightedObject = currentObjectinView[0];
+            this.highlightedObjectCamera = currentObjectinView[0];
             for (int i = 0; i < currentObjectinView.Count - 1; i++)
             {
                 if (CalculateDistance(currentObjectinView[i].X, currentObjectinView[i].Y, car.X, car.Y) <= CalculateDistance(currentObjectinView[i + 1].X, currentObjectinView[i + 1].Y, car.X, car.Y))
                 {
-                    this.highlightedObject = currentObjectinView[i];
+                    this.highlightedObjectCamera = currentObjectinView[i];
                 }
             }
         }
