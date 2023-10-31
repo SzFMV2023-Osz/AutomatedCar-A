cbb3cf199d31110cdbd17c36e395cc2c31b60b9b
Zsebtelep <botosgergo98@gmail.com>
2023-10-31 11:55:44 +0100

    -

--- src/AutomatedCar/SystemComponents/Radar.cs
+++ src/AutomatedCar/SystemComponents/Radar.cs
@@ -16,7 +16,7 @@
 
         public int RadarPositionX { get; set; }
         public int RadarPositionY { get; set; }
-        public WorldObject highlightedObject { get; private set; }
+        
 
         // It's not necessary, should be a protected setter be better? 
         public Radar (double ViewAngle, double ViewDistance) 
@@ -38,21 +38,6 @@
         {
             //this.sensorTriangle
         }
-        public void ClosestHighlightedObject(AutomatedCar car)
-        {
-            this.highlightedObject = currentObjectinView[0];
-            for (int i = 0; i < currentObjectinView.Count - 1; i++)
-            {
-                if (CalculateDistance(currentObjectinView[i].X, currentObjectinView[i].Y, car.X, car.Y) <= CalculateDistance(currentObjectinView[i + 1].X, currentObjectinView[i + 1].Y, car.X, car.Y))
-                {
-                    this.highlightedObject = currentObjectinView[i];
-                }
-            }
-        }
-        private double CalculateDistance(double xACoordinate, double yACoordinate, double xBCoordinate, double yBCoordinate)
-        {
-            double distance = Math.Sqrt(Math.Pow((xBCoordinate - xACoordinate), 2) + Math.Pow((yBCoordinate - yACoordinate), 2));
-            return distance;
-        }
+        
     }
 }
