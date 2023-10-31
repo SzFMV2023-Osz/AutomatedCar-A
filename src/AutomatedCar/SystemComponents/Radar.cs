6772323f31888259b81f871026ea56c18fabeb7f
gegeny.attila <gegeny.attila@stud.uni-obuda.hu>
2023-10-31 15:52:44 +0100

    breakinfos logic reworked

--- src/AutomatedCar/SystemComponents/Radar.cs
+++ src/AutomatedCar/SystemComponents/Radar.cs
@@ -1,8 +1,10 @@
 ﻿namespace AutomatedCar.SystemComponents
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using AutomatedCar.Models;
+    using DynamicData;
 
     internal class Radar : Sensor
     {
@@ -22,12 +24,84 @@
         public override void Process()
         {
             this.ObjectsinViewUpdate(World.Instance.WorldObjects);
+            this.RemoveObjectsNotinView();
+            this.RefreshDistances();
+            this.RefreshPreviousObjects();
         }
 
         //figure out how the PolylineGeometry works
         public void VisualiseRadarVision()
         {
             //this.sensorTriangle
+        }
+
+        // Refreshes the distance of elements in previousObjectinView List
+        private void RefreshDistances()
+        {
+            foreach (RelevantObject prevobj in this.previousObjectinView)
+            {
+                prevobj.modifyPreviousDistance(prevobj.CurrentDistance);
+                prevobj.modifyCurrentDistance(this.CalculateDistance(prevobj.RelevantWorldObject.X, prevobj.RelevantWorldObject.Y, 0, 0));
+            }
+        }
+
+        // Refreshes previousObjectinView List regarding the elements (excluding roads)
+        private void RefreshPreviousObjects()
+        {
+            // végig megyünk a mostani objecteken, és ha nem találunk egyet
+            // a korábbi objectek listájában, hozzáadjuk
+            foreach (WorldObject WO in this.currentObjectinView)
+            {
+                if (!WO.WorldObjectType.Equals(WorldObjectType.Road))
+                {
+                    bool mustadd = true;
+                    foreach (RelevantObject prevobj in this.previousObjectinView)
+                    {
+                        if (prevobj.RelevantWorldObject.Equals(WO))
+                        {
+                            mustadd = false;
+                            break;
+                        }
+                    }
+
+                    if (mustadd)
+                    {
+                        double distance = this.CalculateDistance(WO.X, WO.Y, 0, 0);
+                        this.previousObjectinView.Add(new RelevantObject(WO, distance, distance));
+                    }
+                }
+            }
+        }
+
+        // Removes objects that are no longer in view from previousObjectinView List
+        private void RemoveObjectsNotinView()
+        {
+            // ha előbb láttuk de most nem, töröljük
+            foreach (RelevantObject prevobj in this.previousObjectinView)
+            {
+                if (!this.currentObjectinView.Contains(prevobj.RelevantWorldObject))
+                {
+                    this.previousObjectinView.Remove(prevobj);
+                }
+            }
+        }
+
+        // Returns relevant objects
+        public List<RelevantObject> RelevantObjects()
+        {
+
+
+            List<RelevantObject> relevantObjects = new List<RelevantObject>();
+
+            foreach (RelevantObject relobj in this.previousObjectinView)
+            {
+                if (relobj.CurrentDistance < relobj.PreviousDistance)
+                {
+                    relevantObjects.Add(relobj);
+                }
+            }
+
+            return relevantObjects;
         }
     }
 }
