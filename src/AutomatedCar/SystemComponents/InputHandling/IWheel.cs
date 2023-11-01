1afcf14e50828a48a1a547650c4193cefc256dd5
SKincso <suranyi.kincso@gmail.com>
2023-11-01 18:02:00 +0100

    Make IntToDegrees() static

--- src/AutomatedCar/SystemComponents/InputHandling/IWheel.cs
+++ src/AutomatedCar/SystemComponents/InputHandling/IWheel.cs
@@ -3,7 +3,5 @@ namespace AutomatedCar.SystemComponents.InputHandling
     public interface IWheel
     {
         public double AngleAsDegree { get; set; }
-
-        public double IntToDegrees(int angleInt);
     }
 }
