a8cde2b37abb73e59858ed051796bfc7abc08ff9
SKincso <suranyi.kincso@gmail.com>
2023-11-01 18:02:49 +0100

    Rename MovementVectorPacket to PowertrainPacket

---
+++ src/AutomatedCar/SystemComponents/Packets/PowertrainPacket.cs
@@ -0,0 +1,22 @@
+﻿namespace AutomatedCar.SystemComponents.Packets
+{
+    using ReactiveUI;
+
+    public class PowertrainPacket : ReactiveObject, IPowertrainPacket
+    {
+        private Vector2 movementVector;
+        private double rotation;
+
+        public Vector2 MovementVector
+        {
+            get => this.movementVector;
+            set => this.RaiseAndSetIfChanged(ref this.movementVector, value);
+        }
+
+        public double Rotation
+        {
+            get => this.rotation;
+            set => this.RaiseAndSetIfChanged(ref this.rotation, value);
+        }
+    }
+}
