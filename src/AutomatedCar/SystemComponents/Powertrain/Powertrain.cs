02ac9f98519e3e69e492480893fe56163971b2a5
DanShepherd27 <jdani20001027@gmail.com>
2023-11-01 17:36:51 +0100

    Created Powertrain class, Engine is no longer a SystemComponent

---
+++ src/AutomatedCar/SystemComponents/Powertrain/Powertrain.cs
@@ -0,0 +1,41 @@
+﻿namespace AutomatedCar.SystemComponents.Powertrain
+{
+    using AutomatedCar.SystemComponents.Engine;
+    using AutomatedCar.SystemComponents.Gearbox;
+    using AutomatedCar.SystemComponents.InputHandling;
+    using AutomatedCar.SystemComponents.Packets;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Powertrain : SystemComponent
+    {
+        public IWheel Wheel { get; set; }
+
+        public IThrottle Throttle { get; set; }
+
+        public IEngine Engine { get; set; }
+
+        public IGearBox GearBox { get; set; }
+
+        // public MovementVector MovementVector { get; set; }
+
+        public IMovementVectorPacket MovementVectorPacket { get; set; }
+
+        public Powertrain(VirtualFunctionBus virtualFunctionBus) : base (virtualFunctionBus) { 
+            this.Wheel = new Wheel();
+            this.Throttle = new Throttle();
+            this.GearBox = new ATGearBox();
+            this.Engine = new Engine(this.GearBox, this.Throttle);
+            this.MovementVectorPacket = new MovementVectorPacket();
+
+        }
+
+        public override void Process()
+        {
+            (this.MovementVectorPacket as MovementVectorPacket).MovementVector = (0, 0);
+        }
+    }
+}
