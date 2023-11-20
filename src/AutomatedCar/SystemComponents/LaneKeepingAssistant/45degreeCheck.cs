0d49963c9bb9a44dcd9f7cb6fa16ea5785aae7f4
Zsebtelep <botosgergo98@gmail.com>
2023-11-20 17:03:17 +0100

    Create 45degreeCheck.cs

---
+++ src/AutomatedCar/SystemComponents/LaneKeepingAssistant/45degreeCheck.cs
@@ -0,0 +1,12 @@
+﻿namespace AutomatedCar.SystemComponents.LaneKeepingAssistant
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class _45degreeCheck
+    {
+    }
+}
