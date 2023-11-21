4ae717722b462cd0c6c758c997f409d5bf79770a
VELOC1Ty01 <laloka011996@gmail.com>
2023-11-21 15:03:30 +0100

    Created inputPackets

---
+++ src/AutomatedCar/SystemComponents/Packets/InputPackets/IReadOnlyInputPacket.cs
@@ -0,0 +1,26 @@
+﻿namespace AutomatedCar.SystemComponents.Packets.InputPackets
+{
+    using AutomatedCar.Helpers.Gearbox_helpers;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IReadOnlyInputPacket
+    {
+        int? BrakePercentage { get; }
+
+        int? ThrottlePercentage { get; }
+
+        double? WheelPercentage { get; }
+
+        SequentialShiftingDirections? ShiftUpOrDown { get; }
+    }
+
+    //public interface IReadOnlyAEBInputPacket : IReadOnlyInputPacket { }
+
+    //public interface IReadOnlyLCCInputPacket : IReadOnlyInputPacket { }
+
+    //public interface IReadOnlyLKAInputPacket : IReadOnlyInputPacket { }
+}
---
+++ src/AutomatedCar/SystemComponents/Packets/InputPackets/LKAInputPacket.cs
@@ -0,0 +1,66 @@
+﻿namespace AutomatedCar.SystemComponents.Packets.InputPackets
+{
+    using AutomatedCar.Helpers.Gearbox_helpers;
+    using ReactiveUI;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class LKAInputPacket : ReactiveObject, IReadOnlyInputPacket
+    {
+        public LKAInputPacket() {
+            this.throttlePercentage = null;
+            this.brakePercentage = null;
+            this.wheelPercentage = null;
+            this.shiftUpOrDown = null;
+        }
+        /// <summary>
+        /// Represents how far the brake pedal is being pushed down. 0 meaning not at all, 100 meaning fully pressed.
+        /// </summary>
+        private int? brakePercentage;
+
+        /// <summary>
+        /// Represents how far the throttle pedal is being pushed down. 0 meaning not at all, 100 meaning fully pressed.
+        /// </summary>
+        private int? throttlePercentage;
+
+        /// <summary>
+        /// Represents steering wheel rotation between values -100 and +100 (left to right). 0 means that the wheel is centered.
+        /// </summary>
+        private double? wheelPercentage;
+
+        /// <summary>
+        /// Value can be:
+        /// -1 meaning shift down,
+        /// 0 meaning don't shift,
+        /// +1 meaning shift up.
+        /// </summary>
+        private SequentialShiftingDirections? shiftUpOrDown;
+
+        public int? BrakePercentage
+        {
+            get => brakePercentage;
+            set => this.RaiseAndSetIfChanged(ref brakePercentage, value);
+        }
+
+        public int? ThrottlePercentage
+        {
+            get => throttlePercentage;
+            set => this.RaiseAndSetIfChanged(ref throttlePercentage, value);
+        }
+
+        public double? WheelPercentage
+        {
+            get => wheelPercentage;
+            set => this.RaiseAndSetIfChanged(ref wheelPercentage, value);
+        }
+
+        public SequentialShiftingDirections? ShiftUpOrDown
+        {
+            get => shiftUpOrDown;
+            set => this.RaiseAndSetIfChanged(ref shiftUpOrDown, value);
+        }
+    }
+}
