edac3edcbf6ecd308ffd3b1ccfbf71f477cae262
SKincso <suranyi.kincso@gmail.com>
2023-11-30 20:17:40 +0100

    Add AEB class

---
+++ src/AutomatedCar/SystemComponents/ADAS/AEB.cs
@@ -0,0 +1,24 @@
+﻿namespace AutomatedCar.SystemComponents.ADAS
+{
+    using System;
+    using System.Collections.Generic;
+    using AutomatedCar.SystemComponents.Packets.InputPackets.DriveAssistPackets;
+    using AutomatedCar.Models;
+
+    class AEB : SystemComponent
+    {
+        public AEBInputPacket AEBPacket { get; set; }
+
+        public AEB(VirtualFunctionBus virtualFunctionBus)
+            : base (virtualFunctionBus)
+        {
+            this.AEBPacket = new AEBInputPacket();
+            this.virtualFunctionBus.AEBInputPacket = this.AEBPacket;
+        }
+
+        public override void Process()
+        {
+            List<RelevantObject> relevantObjects = this.virtualFunctionBus.RadarPacket.RelevantObjects;
+        }
+    }
+}
