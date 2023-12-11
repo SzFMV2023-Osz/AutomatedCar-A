74060e919f363e0cb153fb3aa2877df876e11325
DanShepherd27 <jdani20001027@gmail.com>
2023-12-11 22:45:56 +0100

    AEB is activating, but MovementCalculator needs to calculate braking force by deceleration too. Warnings don't work yet.

--- src/AutomatedCar/SystemComponents/ADAS/AEB.cs
+++ src/AutomatedCar/SystemComponents/ADAS/AEB.cs
@@ -1,142 +1,113 @@
 ﻿namespace AutomatedCar.SystemComponents.ADAS
 {
+    using System;
     using System.Collections.Generic;
     using AutomatedCar.Models;
     using AutomatedCar.SystemComponents.Packets.InputPackets.DriveAssistPackets;
-    using System.ComponentModel;
-    using System.Linq;
-    using System;
-
-    public enum AEBStatus
-    {
-        Off,
-        PartialBrake1,
-        PartialBrake2,
-        FullBrake
-    }
+    using ColorTextBlock.Avalonia;
 
     class AEB : SystemComponent
     {
-        private bool activateFCW;
 
-        private AEBStatus status;
+        private const double PixelsToMeters = 1.0 / 50.0; // Conversion factor from pixels to meters
+        private const double FrameTimeSeconds = 1.0 / 60.0; // Time for one frame in seconds
+        private const int MeterToPxRatio = 50;
 
-
-
-        /// <summary>
-        /// First-stage partial braking in m/s2.
-        /// </summary>
-        private const double PB1_DECELERATION = 4;
 
         /// <summary>
-        /// Second-stage partial braking in m/s2.
+        /// Initializes a new instance of the <see cref="AEB"/> class.
         /// </summary>
-        private const double PB2_DECELERATION = 6;
-
-        /// <summary>
-        /// Full braking in m/s2.
-        /// </summary>
-        private const double FB_DECELERATION = 9;
-
-        public AEBInputPacket AEBPacket { get; set; }
-
+        /// <param name="virtualFunctionBus">Virtual function bus.</param>
         public AEB(VirtualFunctionBus virtualFunctionBus)
             : base (virtualFunctionBus)
         {
-            this.AEBPacket = new AEBInputPacket();
-            this.virtualFunctionBus.AEBInputPacket = this.AEBPacket;
+            this.AEBInputPacket = new AEBInputPacket();
+            this.virtualFunctionBus.AEBInputPacket = this.AEBInputPacket;
+            this.IsEmergencyBraking = false;
         }
 
-        int meterToPxRatio = 50;
+        public AEBInputPacket AEBInputPacket { get; set; }
+
+        public bool IsEmergencyBraking { get; set; }
 
         public override void Process()
         {
-            //List<WorldObject> relevantObjects = this.virtualFunctionBus.RelevantObjectsPacket.RelevantObjects?.Where(x => x.Collideable).ToList();
-
-            //We give up...
-
             int egoSpeed = this.virtualFunctionBus.PowertrainPacket.Speed;
             List<RelevantObject> relevantObjects = this.virtualFunctionBus.RelevantObjectsPacket.RelevantObjects;
-            if (relevantObjects?.Count > 0)
+
+            // Deactivate AEB after car stops on an emergency breaking.
+            if (this.IsEmergencyBraking && egoSpeed == 0)
             {
-                foreach (var obj in relevantObjects)
-                {
-                    if (this.status != AEBStatus.Off)
-                    {
-                        break;
-                    }
-
-                    //this.AEBPacket = this.CalculateBrakingForce(
-                    //    obj.CurrentDistance,
-                    //    this.CalculateSpeed(obj.CurrentDistance, obj.PreviousDistance / meterToPxRatio));
-
-                    // calculate speed from previos and current distance
-                    int relativeSpeed = this.CalculateSpeedForRelevantObject(obj);
-                    double driverDeceleration = 0; // to calculate ?
-                    int driverReactionTime = 5;
-                    this.AEBLogic(obj.CurrentDistance, relativeSpeed, egoSpeed, driverDeceleration, driverReactionTime);
-
-                    this.AEBPacket.WarningAvoidableCollision = this.activateFCW;
-                    this.AEBPacket.Status = this.status;
-
-                    switch (this.status)
-                    {
-                        case AEBStatus.PartialBrake1:
-                            // calculate and set AEBPacket.BrakePercentage according to PB1_DECELERATION
-                            break;
-                        case AEBStatus.PartialBrake2:
-                            // calculate and set AEBPacket.BrakePercentage according to PB2_DECELERATION
-                            break;
-                        case AEBStatus.FullBrake:
-                            // calculate and set AEBPacket.BrakePercentage according to FB_DECELERATION
-                            break;
-                        default:
-                            break;
-                    }
-                }
+                    this.IsEmergencyBraking = false;
             }
 
-            this.AEBPacket.WarningOver70kmph = egoSpeed > 70 ? true : false;
+            if (!this.IsEmergencyBraking)
+            {
+                this.AEBInputPacket.BrakePercentage = null;
+                this.AEBInputPacket.ThrottlePercentage = null;
+            }
+
+            if (relevantObjects?.Count > 0 && egoSpeed <= 70 && egoSpeed > 0)
+            {
+                AEBInputPacket result = new AEBInputPacket();
+                foreach (var obj in relevantObjects)
+                {
+                     result = this.CalculateBrakingForce(
+                        obj.CurrentDistance,
+                        this.CalculateSpeed(obj.CurrentDistance, obj.PreviousDistance));
+                }
+
+                if ((this.AEBInputPacket.BrakePercentage == null && result.BrakePercentage > 0) || this.AEBInputPacket.BrakePercentage < result.BrakePercentage)
+                {
+                    this.AEBInputPacket.BrakePercentage = result.BrakePercentage;
+                    this.AEBInputPacket.ThrottlePercentage = 0;
+                }
+            }
+            else
+            {
+                // WARNING OVER 70km/h
+                this.AEBInputPacket.WarningOver70kmph = true;
+            }
         }
 
+        /// <summary>
+        /// Calculates braking force.
+        /// </summary>
+        /// <param name="distanceInPixels">Distance from object in question in pixels.</param>
+        /// <param name="currentSpeed">Relative speed compared to object in question (m/s).</param>
+        /// <returns>AEBInputPacket that the powertrain can use.</returns>
         public AEBInputPacket CalculateBrakingForce(double distanceInPixels, double currentSpeed)
         {
-            var distance = distanceInPixels * PixelsToMeters;
+            var distanceInMeters = distanceInPixels * PixelsToMeters;
 
             AEBInputPacket result = new AEBInputPacket();
 
-            // Convert speed from km/h to m/s
-            double speedMetersPerSecond = currentSpeed * 1000 / 3600;
+            // Calculate the deceleration required to stop before hitting the object
+            double deceleration = Math.Pow(currentSpeed, 2) / (2 * distanceInMeters);
 
-            // Check if current speed exceeds 70 km/h
-            if (currentSpeed <= 70)
+            // If the deceleration required is greater than the maximum deceleration possible (9 m/s^2), limit it
+            if (deceleration > 9)
             {
-                // Calculate the deceleration required to stop before hitting the object
-                double deceleration = Math.Pow(speedMetersPerSecond, 2) / (2 * distance);
+                deceleration = 9;
+            }
 
-                // If the deceleration required is greater than the maximum deceleration possible (9 m/s^2), limit it
-                if (deceleration > 9)
-                {
-                    deceleration = 9;
-                    result.WarningAvoidableCollision = true;
-                }
+            // FIXME
+            result.WarningAvoidableCollision = true;
 
+            // Activate AEB at 7 m/s^2 deceleration
+            if (deceleration > 7)
+            {
                 // Calculate the braking force
                 double brakingForce = deceleration * 100 / 9; // Scale to a range of 0-100
 
                 // Update the brake percentage in the result packet
                 result.BrakePercentage = (int)brakingForce;
-            }
-            else
-            {
-                result.WarningOver70kmph = true;
+
+                this.IsEmergencyBraking = true;
             }
 
             return result;
         }
-
-        private const double PixelsToMeters = 1.0 / 50.0; // Conversion factor from pixels to meters
-        private const double FrameTimeSeconds = 1.0 / 60.0; // Time for one frame in seconds
 
         public double CalculateSpeed(double currentDistance, double previousDistance)
         {
@@ -150,73 +121,6 @@
             double speed = distanceChangeMeters / FrameTimeSeconds;
 
             return speed;
-        }
-
-        public double CalculateDistance(int x1, int y1, int x2, int y2)
-        {
-            // Calculate distance using Euclidean distance formula
-            double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            return distance;
-        }
-
-        private int CalculateSpeedForRelevantObject(RelevantObject relevantObject)
-        {
-            return (int)((relevantObject.PreviousDistance - relevantObject.CurrentDistance) / (1 / 60));
-        }
-
-        /// <summary>
-        /// Calculates Time-To-Collision.
-        /// MIO: Most Important Object.
-        /// </summary>
-        /// <param name="mioDistance">Distance from MIO in meters.</param>
-        /// <param name="mioSpeed">Relative speed of MIO in m/s.</param>
-        /// <returns>Time-To-Collision in seconds.</returns>
-        private int CalculateTTC(double mioDistance, int mioSpeed)
-        {
-            return (int)(mioDistance / mioSpeed);
-        }
-
-        internal struct StoppingTime
-        {
-            internal int FCWStoppingTime;
-            internal int PB1StoppingTime;
-            internal int PB2StoppingTime;
-            internal int FBStoppingTime;
-        }
-
-        private StoppingTime CalculateStoppingTimes(int egoSpeed, double driverDeceleration, int driverReactionTime)
-        {
-            StoppingTime stoppingTimes = new StoppingTime();
-
-            stoppingTimes.FCWStoppingTime = (int)(driverReactionTime + (egoSpeed / driverDeceleration));
-            stoppingTimes.PB1StoppingTime = (int)(egoSpeed / PB1_DECELERATION);
-            stoppingTimes.PB2StoppingTime = (int)(egoSpeed / PB2_DECELERATION);
-            stoppingTimes.FBStoppingTime = (int)(egoSpeed / FB_DECELERATION);
-
-            return stoppingTimes;
-        }
-
-        private void AEBLogic(double mioDistance, int mioSpeed, int egoSpeed, double driverDeceleration, int driverReactionTime)
-        {
-            int TTC = this.CalculateTTC(mioDistance, mioSpeed);
-            StoppingTime stoppingTimes = this.CalculateStoppingTimes(egoSpeed, driverDeceleration, driverReactionTime);
-
-            if (TTC < stoppingTimes.PB2StoppingTime)
-            {
-                this.status = AEBStatus.FullBrake;
-            }
-            else if (TTC < stoppingTimes.PB1StoppingTime)
-            {
-                this.status = AEBStatus.PartialBrake2;
-            }
-            else if (TTC < stoppingTimes.FCWStoppingTime - driverReactionTime)
-            {
-                this.status = AEBStatus.PartialBrake1;
-            }
-            else if (TTC < stoppingTimes.FCWStoppingTime)
-            {
-                this.activateFCW = true;
-            }
         }
     }
 }
